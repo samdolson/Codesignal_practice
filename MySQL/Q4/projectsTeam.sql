41ba3a9116304e3ff046dca99250866e06e1c271
samdolson <samuel.david.olson@gmail.com>
2021-08-26 09:05:09 -0700

    initial MySQL answers

---
+++ MySQL/Q4/projectsTeam.sql
@@ -0,0 +1,7 @@
+CREATE PROCEDURE projectsTeam()
+BEGIN
+    /* Write your SQL here. Terminate each statement with a semicolon. */
+    SELECT DISTINCT name 
+    FROM projectLog
+    ORDER BY name ASC;
+END
